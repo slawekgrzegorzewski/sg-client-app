7349d340a22ebb59b372c0c6ecc8e118e2b65e12
slawek <slawek.grz@gmail.com>
2020-08-19 21:01:34 +0200

    Users accounts adjustments

--- src/app/components/accounts/user-accounts.component.ts
+++ src/app/components/accounts/user-accounts.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {Account} from "../../model/account";
 import {Button} from "../gui/hoverable-buttons.component";
 
@@ -13,13 +13,10 @@ export class UserAccountsComponent implements OnInit {
   @Input() buttons: Button<Account>[];
 
   private _accounts: Account[];
+
   @Input() set accounts(value: Account[]) {
     this._accounts = value;
-    this.totalBalancesPerCurrency = (this.accounts || [])
-      .reduce(
-        (map, acc) => map.set(acc.currency, (map.get(acc.currency) || 0) + acc.currentBalance),
-        new Map<string, number>()
-      )
+    this.recalculateSubtotals();
   }
 
   get accounts(): Account[] {
@@ -27,18 +24,12 @@ export class UserAccountsComponent implements OnInit {
   }
 
   totalBalancesPerCurrency: Map<string, number>
-  _overElement: Account;
-  get overElement(): Account {
-    return this._overElement;
-  }
 
-  set overElement(value: Account) {
-    this._overElement = value;
-  }
-  a(value: Account) {
-    this._overElement = value;
-  }
-
+  @ViewChild('utilBox') utilBox: ElementRef;
+  overElement: Account;
+  utilBoxTop: number;
+  utilBoxLeft: number;
+  utilBoxVisibility: string = 'hidden';
 
   constructor() {
   }
@@ -50,6 +41,14 @@ export class UserAccountsComponent implements OnInit {
     return a.name + ' - ' + a.currentBalance + ' ' + a.currency;
   }
 
+  private recalculateSubtotals() {
+    this.totalBalancesPerCurrency = (this.accounts || [])
+      .reduce(
+        (map, acc) => map.set(acc.currency, (map.get(acc.currency) || 0) + acc.currentBalance),
+        new Map<string, number>()
+      )
+  }
+
   firstEntry() {
     let next = Array.from(this.totalBalancesPerCurrency.entries())[0];
     return next;
@@ -58,5 +57,18 @@ export class UserAccountsComponent implements OnInit {
   restOfEntries() {
     let next = Array.from(this.totalBalancesPerCurrency.entries()).slice(1);
     return next;
+  }
+
+  setOverAccount(value: Account, accountRow: HTMLTableRowElement) {
+    this.overElement = value;
+    console.log(value ? value.id : 'leave')
+    if (value) {
+      var adjustment = (accountRow.offsetHeight - this.utilBox.nativeElement.offsetHeight) / 2;
+      this.utilBoxTop = accountRow.getBoundingClientRect().top + adjustment;
+      this.utilBoxLeft = accountRow.getBoundingClientRect().left + accountRow.clientWidth - this.utilBox.nativeElement.offsetWidth;
+      this.utilBoxVisibility = 'visible'
+    } else {
+      this.utilBoxVisibility = 'hidden'
+    }
   }
 }
