83fabd380c6a719abff50b2b5fa77b9fa222c209
slag <slawek.grz@gmail.com>
2021-10-08 19:09:22 +0200

    update ip address

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // serviceUrl: 'http://localhost:8080'
-  serviceUrl: 'http://172.18.242.17:8080'
+  serviceUrl: 'http://172.19.89.221:8080'
 };
 
 /*
