eb07e405a03301e11607bb56c431902f4aeb63f7
slawek <slawek.grz@gmail.com>
2022-09-19 00:59:25 +0200

    Dockerize

--- dev-scripts/buildAndPushImage.ps1
+++ dev-scripts/buildAndPushImage.ps1
@@ -1,9 +1,9 @@
 npm run buildProd
 
-$env:TAG="slawekgrzegorzewski/client:$args"
-$env:TAG_LATEST="slawekgrzegorzewski/client:latest"
+$env:TAG="215372400964.dkr.ecr.eu-central-1.amazonaws.com/client:$args"
+$env:TAG_LATEST="215372400964.dkr.ecr.eu-central-1.amazonaws.com/client:latest"
 
-pwd
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 215372400964.dkr.ecr.eu-central-1.amazonaws.com
 
 docker buildx build -f docker/Dockerfile --platform linux/amd64 -t $env:TAG -t $env:TAG_LATEST .
 docker push $env:TAG
