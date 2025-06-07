14529af552bb4d7e9d0956c088b879f439936d03
Sławek <slawek.grz@gmail.com>
2025-06-07 21:07:07 +0200

    no longer send domainId in headers

--- src/app/general/interceptors/auth.interceptor.ts
+++ src/app/general/interceptors/auth.interceptor.ts
@@ -2,23 +2,19 @@ import {Injectable} from '@angular/core';
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {LoginService} from '../services/login.service';
-import {Router} from '@angular/router';
 import {catchError, map} from 'rxjs/operators';
-import {DomainService} from '../services/domain.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private loginService: LoginService, private domainService: DomainService) {
+  constructor(private loginService: LoginService) {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (this.loginService.getToken()) {
-      const domainId = this.domainService.currentDomainId;
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${this.loginService.getToken()}`,
-          DomainId: domainId ? domainId.toString() : '',
           locale: navigator.language ? navigator.language : 'pl-PL',
           'X-Timezone-Id': Intl.DateTimeFormat().resolvedOptions().timeZone
         }
